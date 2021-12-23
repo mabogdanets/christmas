2bce5b17d398b5cef16f31f84aa812a0cbe89eb4
Marica <maria.bogdanets@gmail.com>
2021-12-23 13:36:53 +0300

    fix: fix limit of bookmark

--- src/ts/marked.ts
+++ src/ts/marked.ts
@@ -6,18 +6,19 @@ let markedNumber: number = 0;
 
 document.querySelectorAll('.toy').forEach(el => {
   el.addEventListener('click', function() {
-    if (markedNumber > 20) {
-      alert("Извините, все слоты заполнены");
-    } else {
-      if (!el.classList.contains('marked')) {
+    if (!el.classList.contains('marked')) {
+      if (markedNumber >= 20) {
+        alert("Извините, все слоты заполнены");
+      } else {
         markedNumber += 1;
         el.classList.add('marked');
         markedCountSpan.textContent = markedNumber.toString();
-      } else {
-        el.classList.remove('marked');
-        markedNumber -= 1;
-        markedCountSpan.textContent = markedNumber.toString();
-      }      
-    }
+      }
+    } else {
+      el.classList.remove('marked');
+      markedNumber -= 1;
+      markedCountSpan.textContent = markedNumber.toString();
+    }      
+    
   });
 });
